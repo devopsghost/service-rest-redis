165fbcc53716293f29c508b9350544cd2b9908ca
ghostnsnow <ghostnsnow@users.noreply.github.com>
2025-01-05 16:47:17 +0530

    create deployments, clusterip services using k8s & use rest calls with cluster.local IP addresses

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17
 RUN mkdir /app
-COPY /target/service-rest-redis-1.0.0.jar /app
-CMD ["java", "-jar", "/app/service-rest-redis-1.0.0.jar"]
+COPY /target/service-rest-redis-2.0.0.jar /app
+CMD ["java", "-jar", "/app/service-rest-redis-2.0.0.jar"]
---
+++ src/main/java/org/chintanu/service_rest_redis/controller/PersonController.java
@@ -0,0 +1,15 @@
+package org.chintanu.service_rest_redis.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/getUser")
+    public ResponseEntity<String> getUser() {
+
+        return ResponseEntity.ok("User 1");
+    }
+}
